ef526a95f61aec3927e52756abd03125073839e1
谭彪 <tanbiao@tanbiaodeMac-mini.local>
2017-10-17 15:06:17 +0800

    第一次提交

--- AlamofireObjectMapper/ViewController.swift
+++ AlamofireObjectMapper/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
+    let param = ["opt_type":1,
+                 "size":20,
+                 "offset":(2 - 1) * 20]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        NetworkTools().imageList(parameters: param) { (result) in
+            
+            switch result
+            {
+                 case .Success( let value):
+                
+                    for goods in value.goods_list!
+                    {
+                        print(goods)
+                        print("乡村:" + goods.country)
+                        print("商品名:" + goods.goods_name)
+                        print("Url :" + goods.hd_thumb_url)
+                        
+                        guard let group = goods.group else {
+                            print("..............")
+                            return
+                        }
+                       
+                        print("group :\(String(describing: group.customer_num))")
+                        print("group : \(String(describing: group.price))")
+                        //
+                        print("..............")
+                    }
+                    
+                    print("data:\(value)")
+                
+                 case .HttpError(error: let errorMsg):
+                    print("错误信息:\(errorMsg)")
+                
+                 default:
+                    
+                   break
+            }
+        }
+         
+            
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+     
     }
-
 
 }
 
